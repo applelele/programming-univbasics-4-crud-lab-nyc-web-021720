535c88ebe0e0500dd59e2e97dcae4cb5155e4c5d
YUKIKO IKEDA <applelele@gmail.com>
2020-01-21 08:45:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,17 +27,21 @@ end
 remove_element_from_start_of_array(["Where", "you" "are", "going"])
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
+retrieve_element_from_index(["Michael", "Jeff", "Ryan", "Adam"], 2)
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
+retrieve_first_element_from_array(["Ashley", "Margaret", "Linda", "Kimberly"])
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
+retrieve_last_element_from_array(["small", "medium", "large"])
 
 def update_element_from_index(array, index_number, element)
-
+ array[index_number] = element
 end
+update_element_from_index(["bag", "coat", "shoes", "umbrella"], 3, "sweater")
