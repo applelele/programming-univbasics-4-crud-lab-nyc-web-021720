d7bb42b2a30baa321b7a234a9f396220439708ad
YUKIKO IKEDA <applelele@gmail.com>
2020-01-21 08:40:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,8 +22,9 @@ end
 remove_element_from_end_of_array(["cat", "dog", "rabbit", "elefant"])
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
+remove_element_from_start_of_array(["Where", "you" "are", "going"])
 
 def retrieve_element_from_index(array, index_number)
   
